7bca6baf0b7e8bb7f3a3f47a33cb9e3c13ce90db
Jan-Marcus Sivadi <jmarcus@hot.ee>
2023-03-15 16:44:07 +0200

    init

---
+++ GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using SolarPlanets.Views;
+global using System.ComponentModel;
+
+global using System;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Hosting;
+global using Microsoft.Maui.Controls.Hosting;
